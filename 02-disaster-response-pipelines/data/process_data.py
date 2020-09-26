6e13a38b874881e15281378cb795f844f29c0f89
neilpmayhew <neil@sapphiresoftware.co>
2020-09-26 11:17:12 +0100

    fix: add if_exists='replace' to to_sql function call so that if the table already exists in the db it will not fail.

--- 02-disaster-response-pipelines/data/process_data.py
+++ 02-disaster-response-pipelines/data/process_data.py
@@ -70,7 +70,7 @@ def save_data(df, database_filename):
         filename to save the SQLLite db to
     """
     engine = create_engine(f'sqlite:///{database_filename}')
-    df.to_sql('message_category', engine, index=False)
+    df.to_sql('message_category', engine, index=False, if_exists='replace')
     
 def main():
     if len(sys.argv) == 4:
